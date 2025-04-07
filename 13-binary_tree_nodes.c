c88e3ee51f706790adcd6db038b1902f8d17ff36
Your Name <you@example.com>
2025-04-07 07:23:34 +0000

    T

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree->left != NULL || tree->right != NULL)
 		return (1 + binary_tree_nodes(tree->left) +
-		            binary_tree_nodes(tree->right));
+			binary_tree_nodes(tree->right));
 
 	return (0);
 }
